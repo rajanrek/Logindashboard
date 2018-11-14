9c6f6d01b5084fa8aba7b0e18c32a38051b778a9
Rajan Rek <rajankumar148@gmail.com>
2018-11-14 15:38:32 +0530

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -7,6 +7,8 @@ import { SignUpComponent } from './user/sign-up/sign-up.component';
 import { SignInComponent } from './user/sign-in/sign-in.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 import { AppRoutingModule } from './routs';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -17,6 +19,18 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { ChartsModule } from 'ng2-charts';
 import { AgmCoreModule } from '@agm/core';
+
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GallaryComponent } from './gallary/gallary.component';
+import {MatIconModule} from '@angular/material/icon';
+import { LeftnavComponent } from './leftnav/leftnav.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,18 +43,28 @@ import { AgmCoreModule } from '@agm/core';
     CusdirectvDirective,
     UsersignComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    GallaryComponent,
+    LeftnavComponent
   ],
   imports: [
     BrowserModule,
+    MatExpansionModule,
     AppRoutingModule,
     FormsModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule,
     ChartsModule,
     AgmCoreModule.forRoot({
       // please get your own API key here:
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
       apiKey: 'AIzaSyAvcDy5ZYc2ujCS6TTtI3RYX5QmuoV8Ffw'
-    })
+    }),
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- routs.ts
+++ routs.ts
@@ -8,23 +8,32 @@ import { UsersignComponent } from './clients/usersign.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
+import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
 {path: '', redirectTo: '/signin', pathMatch: 'full'},
 {path: 'signin', component: SignInComponent},
 {path : 'signup' , component: SignUpComponent},
+
+
 {path: 'forgetpass', component: ForgetpassComponent},
 {path: 'sidebar', component: SidebarComponent,
 children: [
   {path: 'home', component: HomeComponent},
   {path: 'client', component: UsersignComponent },
   {path: 'contact', component: ContactComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'gallary', component: GallaryComponent},
+  {path: 'leftnav', component: LeftnavComponent},
+
+
 ]
 
 },
 
-{path: 'client', component: UsersignComponent},
+// {path: 'client', component: UsersignComponent},
 {path: 'chart', component: HomeComponent}
 ];
 
